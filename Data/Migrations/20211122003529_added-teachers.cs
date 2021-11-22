615d05d0ff83c50501bfdb54ecdcd60761a87df5
steli <steli@PEGOS>
2021-11-22 02:44:52 +0200

    Add project files.

---
+++ Data/Migrations/20211122003529_added-teachers.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SchoolManager.Data.Migrations
+{
+    public partial class addedteachers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "Grade",
+                table: "Student",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "FailedSubjects",
+                table: "Student",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "SickHours",
+                table: "Student",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Teachers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SurName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DoB = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Gender = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Phone = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FatherName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    MotherName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Region = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SchoolName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Subject = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    WorkHours = table.Column<int>(type: "int", nullable: false),
+                    SickHours = table.Column<int>(type: "int", nullable: false),
+                    isPrincipal = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Teachers", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Teachers");
+
+            migrationBuilder.DropColumn(
+                name: "FailedSubjects",
+                table: "Student");
+
+            migrationBuilder.DropColumn(
+                name: "SickHours",
+                table: "Student");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Grade",
+                table: "Student",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "float");
+        }
+    }
+}
---
+++ Models/School.cs
@@ -0,0 +1,17 @@
+﻿namespace SchoolManager.Models
+{
+    public class School
+    {
+        public int Id { get; set; }
+        public string SchoolName { get; set; }
+        public long SchoolID { get; set; }
+        public string Region { get; set; }
+        public int Teachers { get; set; }
+        public int Vacancies { get; set; }
+
+        public School()
+        {
+
+        }
+    }
+}
